5bc092d6ce2b5120059482a55904aa78d8a4f802
steven <stevenmakuyana94@gmail.com>
2021-04-11 13:33:58 -0700

    activated web camera and displayed camera info to the user

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
-console.log('hello')
+const camera = document.getElementById('camera')
+const cameraDetails = document.getElementById('camera-details')
+
+const constraint = {
+  audio: false,
+  video: true
+}
+
+navigator.mediaDevices.getUserMedia(constraint)
+.then(mediaStream => {
+  camera.srcObject = mediaStream
+  camera.onloadeddata = (event) => {
+    camera.play()
+  }
+  const tracks = mediaStream.getTracks()[0]
+  const detailsComponent = `
+    <div>
+     <label>label: <span>${tracks.label}</span></label>
+     <label>status: <span>${tracks.readyState}</span></label>
+    </div>
+  `
+  cameraDetails.innerHTML = detailsComponent
+})
